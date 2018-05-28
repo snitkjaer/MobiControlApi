557fde89777d1e201de047b1117d1aabd34fb6e6
snitkjaer <snitkjaer@gmail.com>
2018-05-28 16:58:04 +0200

    Added SendActionToDevicesAsync

--- Shared/Api.cs
+++ Shared/Api.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 
 namespace MobiControlApi.Shared
@@ -66,30 +68,33 @@ namespace MobiControlApi.Shared
 			    return null;
         }
 
-        /*
-		public async Task<string> PutJsonAsync(string resourcePath)
+        
+		public async Task<bool> PostJsonAsync(string resourcePath, string body)
         {
             string token = await authentication.GetAuthenticationToken();
             if (token == null)
-                return null;
+                return false;
 
             httpClient.DefaultRequestHeaders.Clear();
             httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
 
             var request = new HttpRequestMessage
             {
-                Method = HttpMethod.Put,
-                RequestUri = new Uri(config.baseUri, resourcePath)
+				Method = HttpMethod.Post,
+                RequestUri = new Uri(config.baseUri, resourcePath),
+				Content = new StringContent(body),
+                
+                
             };
+			request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json") { CharSet = "UTF-8" };
 
             HttpResponseMessage response = await httpClient.SendAsync(request);
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsStringAsync();
+			if (response.IsSuccessStatusCode)
+				return true;
             else
-                return null;
+                return false;
         }
-        */
-
+        
 
         #endregion
 
@@ -106,6 +111,62 @@ namespace MobiControlApi.Shared
 
            
         }
+
+
+		public async Task<bool> SendActionToDevicesAsync(string deviceId,string Action, string Message)
+        {
+			// POST /devices/actions/DeviceIds
+
+			List<string> deviceIds = new List<string>();
+			deviceIds.Add(deviceId);
+            
+
+			ActionBody actionBody = new ActionBody(deviceIds, new ActionInfo(Action, Message));
+
+
+            // Generate resourcePath
+			string resourcePath = "devices/actions";
+			string actionBodyJson = actionBody.ToJsonString();
+            // Call GetJsonAsync
+			return await PostJsonAsync(resourcePath, actionBodyJson);
+
+
+        }
+
+
+
+
+
               
     }
+
+	public class ActionBody
+    {
+		public List<string> DeviceIds;
+		public ActionInfo ActionInfo;
+
+		public ActionBody(List<string> deviceIds, ActionInfo actionInfo)
+		{
+			DeviceIds = deviceIds;
+			ActionInfo = actionInfo;
+		}
+
+		public string ToJsonString()
+		{
+			return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+		}
+
+	}
+   
+	public class ActionInfo
+    {
+		public string Action;
+		public string Message;
+
+		public ActionInfo(string action, string message)
+		{
+			Action = action;
+			Message = message;
+		}
+	}
 }
---
+++ Shared/RefitApi.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MobiControlApi.Shared
+{
+    public class RefitApi
+    {
+        public RefitApi()
+        {
+        }
+    }
+}
