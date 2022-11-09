2f993b5e65bd1abde0d6ecc9f8566f5f8aabca96
Bo Snitkjær Nielsen <snitkjaer@gmail.com>
2022-11-09 10:45:34 +0100

    Create SendActionToDevicesAsyncTest.cs

---
+++ MobiControlApi.UnitTestMs/Devices/SendActionToDevicesAsyncTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MobiControlApi.UnitTestMs.Devices
+{
+    [TestClass]
+    public class SendActionToDevicesAsyncTest
+    {
+        static CancellationToken token;
+        static MobiControlApiConfig mobiControlApiConfig = MobiControlApiConfig.GetConfigFromJsonFile("MobiControlServerApiConfig.json");
+        HttpClient httpClient = new HttpClient();
+
+        [DataTestMethod]
+        [DataRow(TestData.deviceImei)]
+        public async Task SendActionToDevicesAsyncTest_SyncFilesNows(string imei)
+        {
+            #region Arrange
+            Api mcApi = new Api(mobiControlApiConfig, null, token, httpClient);
+
+            #endregion
+
+            #region Act
+            BasicDevice basicDevice = await mcApi.GetBasicDeviceOnImeiAsync(imei);
+            bool result = await mcApi.SendActionToDevicesAsync(basicDevice.DeviceId, Api.ActionType.SyncFilesNow);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(result);
+            #endregion
+        }
+
+
+        [DataTestMethod]
+        [DataRow(TestData.deviceImei)]
+        public async Task SendActionToDevicesAsyncTest_CheckIn(string imei)
+        {
+            #region Arrange
+            Api mcApi = new Api(mobiControlApiConfig, null, token, httpClient);
+
+            #endregion
+
+            #region Act
+            BasicDevice basicDevice = await mcApi.GetBasicDeviceOnImeiAsync(imei);
+            bool result = await mcApi.SendActionToDevicesAsync(basicDevice.DeviceId, Api.ActionType.CheckIn);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(result);
+            #endregion
+        }
+
+        // // Install client certificate on device
+        [DataTestMethod]
+        [DataRow(TestData.deviceImei, TestData.p12FilePath, TestData.certificatePassword)]
+        public async Task InstallCertificateOnDevice(string imei, string p12FilePath, string certificatePassword)
+        {
+            #region Arrange
+            Api mcApi = new Api(mobiControlApiConfig, null, token, httpClient);
+
+            #endregion
+
+            #region Act
+            BasicDevice basicDevice = await mcApi.GetBasicDeviceOnImeiAsync(imei);
+            bool result = await mcApi.InstallCertificateOnDevice(basicDevice.DeviceId, p12FilePath, certificatePassword);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(result);
+            #endregion
+        }
+
+
+    }
+}
