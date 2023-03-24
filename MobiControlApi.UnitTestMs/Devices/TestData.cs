febfa14ccef63b2def3d70b2d206b1c9b4e66b9b
Bo Snitkjær Nielsen <snitkjaer@gmail.com>
2023-03-24 22:11:14 +0100

    Dependency update

--- MobiControlApi.UnitTestMs/Devices/TestData.cs
+++ MobiControlApi.UnitTestMs/Devices/TestData.cs
@@ -5,5 +5,9 @@ namespace MobiControlApi.UnitTestMs.Devices
     {
         public const string groupName = "/MARK/BUC";
         public const string deviceImei = "353857080569110";
+        public const string p12FilePath = "/sdcard/Download/cart.pfx";
+        public const string certificatePassword = "12345";
+        
+
     }
 }
