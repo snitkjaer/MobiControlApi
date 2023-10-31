952ca048643e739d8bfe78f48c8812eac2173eec
snitkjaer <snitkjaer@gmail.com>
2023-10-31 20:32:17 +0100

    Create RenewDeviceCertificate.cs

---
+++ MobiControlApi.Lib/Devices/DeviceCertificates/RenewDeviceCertificate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.ApplicationInsights.DataContracts;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+using System.Runtime.ConstrainedExecution;
+
+namespace MobiControlApi.Devices.DeviceCertificates
+{
+	public class RenewDeviceCertificate : LogAbstraction
+    {
+        private Api api;
+
+        public RenewDeviceCertificate(Api api)
+        {
+            this.api = api;
+        }
+
+        private async Task<bool> Renew(string deviceId, string referenceId)
+        {
+            // Generate resourcePath
+            // /devices/{deviceId}/certificates/{referenceId}/actions
+            string resourcePath = $"/devices/{deviceId}/certificates/{referenceId}/actions";
+            string action = "{\n  \"ActionKind\": \"Renew\"\n}";
+
+
+            Log($"Renewing device certificate {referenceId} for device {deviceId}", SeverityLevel.Verbose);
+
+            // Call GetJsonAsync
+            return await api.PutJsonAsync(resourcePath, action);
+        }
+    }
+}
+
